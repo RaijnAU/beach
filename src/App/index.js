5410894f67cbe2f3587861a7bfbc0d06b1185319
Ryan Jones <jonesy_222@hotmail.com>
2019-04-22 15:00:41 -0400

    setup-files-complete

---
+++ src/App/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './index.css';
+
+export default () => {
+  return (
+    <div>
+      <header id="showcase">
+        <h1>Welcome To The Beach</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet tempore iure possimus dolorum esse dolor.</p>
+        <a href="#section-a" className="button">Read More</a>
+    </header>
+    <section id="section-a">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam expedita nulla iste maxime repellat porro facere cum molestias adipisci ipsum veniam quo asperiores, eos deserunt temporibus consequuntur! Reiciendis facilis officia similique, repellat officiis a omnis quos soluta quasi ducimus corrupti, est quo. Ad illo sapiente aspernatur optio accusamus saepe impedit deserunt libero cum pariatur, laboriosam cumque magnam eveniet laudantium dolor consequatur molestias quas porro corporis! Ducimus in veritatis, dolorem et eos harum magnam voluptatibus cupiditate eveniet delectus modi maiores blanditiis recusandae nisi repudiandae? Ad impedit dignissimos esse autem nesciunt eligendi beatae repudiandae commodi blanditiis eos dolorem dolores suscipit, ipsa ratione.</p>
+    </section>
+    <section id="section-b">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam expedita nulla iste maxime repellat porro facere cum molestias adipisci ipsum veniam quo asperiores, eos deserunt temporibus consequuntur! Reiciendis facilis officia similique, repellat officiis a omnis quos soluta quasi ducimus corrupti, est quo. Ad illo sapiente aspernatur optio accusamus saepe impedit deserunt libero cum pariatur, laboriosam cumque magnam eveniet laudantium dolor consequatur molestias quas porro corporis! Ducimus in veritatis, dolorem et eos harum magnam voluptatibus cupiditate eveniet delectus modi maiores blanditiis recusandae nisi repudiandae? Ad impedit dignissimos esse autem nesciunt eligendi beatae repudiandae commodi blanditiis eos dolorem dolores suscipit, ipsa ratione.</p>
+    </section>
+    <section id="section-c">
+        <div className="box-1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum corporis eveniet recusandae quaerat eligendi exercitationem aliquid quibusdam minus obcaecati deleniti.
+        </div>
+        <div className="box-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum corporis eveniet recusandae quaerat eligendi exercitationem aliquid quibusdam minus obcaecati deleniti.
+        </div>
+        <div className="box-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum corporis eveniet recusandae quaerat eligendi exercitationem aliquid quibusdam minus obcaecati deleniti.
+        </div>
+    </section>
+
+    </div>
+  )
+}
